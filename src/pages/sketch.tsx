9c5677473c7a5b21064766ad541a83f411c66908
jacob rodriguez <jacobrdz47@gmail.com>
2023-08-16 23:36:09 -0500

    temporary disable of sketch

--- src/pages/sketch.tsx
+++ src/pages/sketch.tsx
@@ -2,12 +2,11 @@ import Sketch from "@/components/sketch/Sketch";
 import { useEffect, useState } from "react";
 
 const SketchPage = () => {
-  const [isMounted, setIsMounted] = useState(false);
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  return <>{isMounted && <Sketch />}</>;
+  // const [isMounted, setIsMounted] = useState(false);
+  // useEffect(() => {
+  //   setIsMounted(true);
+  // }, []);
+  // return <>{isMounted && <Sketch />}</>;
 };
 
 export default SketchPage;
