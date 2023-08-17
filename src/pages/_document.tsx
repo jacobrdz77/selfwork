88ba387997b0656d4da6c2552330ebc1dfece9ab
jacob rodriguez <jacobrdz47@gmail.com>
2023-08-17 03:19:37 -0500

    Added google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -7,6 +8,23 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+
+        <Script
+          strategy="afterInteractive"
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ID}`}
+        ></Script>
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: ` window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ID}'); `,
+          }}
+        />
       </body>
     </Html>
   );
